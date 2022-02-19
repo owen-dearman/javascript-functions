390498fd12011a7f008e9ace46fd5a1bb862d776
Owen Dearman <owendearman@gmail.com>
2022-02-19 15:49:28 +0000

    convertLetterNumber.js code improved

--- convertLetterNumber.js
+++ convertLetterNumber.js
@@ -2,14 +2,15 @@ function alphabetPosition(text) {
     let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     let code = [];
     text = text.toLowerCase();
-    for (let i = 0; i < text.length; i++){
-        let letter = text[i];
+    for (let letter of text){
         if ((/[a-zA-Z]/).test(letter) === true){
             let number = alphabet.indexOf(letter);
             code.push(number+1);
+        } else if (letter === " "){
+            code.push("_")
         }
     }
-    let answer = code.join("");
+    let answer = code.join("_");
     return answer;
   }
 
